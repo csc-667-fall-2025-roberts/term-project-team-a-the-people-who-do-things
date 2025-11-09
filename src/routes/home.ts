a7c64984094f8a2973e3bd037c678301c784f315
Ben Klein <benmklein@gmail.com>
2025-11-09 07:57:49 -0800

    fix npm scripts, folder structure, cleaning

--- src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (_request, response) => {
-  response.render("home");
-});
-
-export default router;
---
+++ src/types/express.d.ts
@@ -0,0 +1,10 @@
+import "express";
+import type { User } from "./user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
