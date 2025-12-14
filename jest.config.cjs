1be8c3ea38ca89ff74bc9bcf67527adbfbe9b3cc
franco <fbarraavila@arizona.edu>
2025-12-13 21:23:52 -0800

    fix: Configure Jest for ESM support

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"], // 1. Treat .ts files as ESM
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true, // 2. Tell ts-jest to compile using ESM settings
+      },
+    ],
   },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
